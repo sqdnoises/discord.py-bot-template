831c758f2cb00229438b7c970991ca43ece08f86
sqd <85015626+sqdnoises@users.noreply.github.com>
2025-03-29 07:51:46 +0530

    Update termcolors.py

--- src/termcolors.py
+++ src/termcolors.py
@@ -18,16 +18,6 @@ The important ones here relate to colour.
 Another way of writing `\\033` would be `\\x1b`.
 """
 
-# ANSI codes are a bit weird to decipher if you're unfamiliar with them, so here's a refresher
-# It starts off with a format like \033[XXXm where XXX is a semicolon separated list of commands
-# The important ones here relate to colour.
-# 30-37 are black, red, green, yellow, blue, magenta, cyan and white in that order
-# 40-47 are the same except for the background
-# 90-97 are the same but "bright" foreground
-# 100-107 are the same as the bright ones but for the background.
-# 0 means reset, 1 means bold, 2 means dim, 3 means italic, 4 means underline and 9 means strikethrough.
-# Another way of writing \033 would be \x1b.
-
 __all__ = (
     # Formatting
     "reset",
@@ -39,6 +29,12 @@ __all__ = (
     # Miscellaneous
     "empty",
     # Custom Colors
+    "ansi",
+    "rgb",
+    "hex",
+    "bg_rgb",
+    "bg_hex",
+    # Custom Predefined Colors
     "lime",
     "orange",
     "dark_orange",
